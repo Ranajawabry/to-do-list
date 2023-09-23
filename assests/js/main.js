4eb60a7da1001998d9cc5932e5f7e2f4f5c1f92b
Ranajawabry <rana.a.jawabry@gmail.com>
2023-09-23 14:51:00 +0300

    Add slug and avoid repeat

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,91 +1,10 @@
-const overlay = document.getElementById("overlay"),
-    task = (window.localStorage || (overlay.classList.add("overlay"), window.alert("sorry your browser dose not support local storege")), document.getElementById("form1")),
-    add = document.getElementById("add"),
-    getTasks = document.getElementById("getTasks"),
-    deleteAllTasks = document.getElementById("deleteAllTasks");
-let tasks;
-tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
-const display = () => {
-        let s = "";
-        for (let t = 0; t < tasks.length; t++)
-            s += `
+const overlay=document.getElementById("overlay"),task=(window.localStorage||(overlay.classList.add("overlay"),window.alert("sorry your browser dose not support local storege")),document.getElementById("form1")),add=document.getElementById("add"),getTasks=document.getElementById("getTasks"),deleteAllTasks=document.getElementById("deleteAllTasks");let tasks;tasks=localStorage.getItem("tasks")?JSON.parse(localStorage.getItem("tasks")):[];const display=()=>{let s="";for(let t=0;t<tasks.length;t++)s+=`
             <tr>
-            <th scope="row">${t + 1}</th>
+            <th scope="row">${t+1}</th>
             <td>${tasks[t].name}</td>
             <td>${tasks[t].status}</td>
             <td>
               <button type="submit" class="btn btn-danger" onclick="deleteTask(${t})">Delete</button>
               <button type="submit" class="btn btn-success ms-1" onclick="finishTask(${t})">Finished</button>
             </td>
-          </tr>`;
-        document.getElementById("data").innerHTML = s;
-    },
-    deleteTask = (t) => {
-        tasks.splice(t, 1), localStorage.setItem("tasks", JSON.stringify(tasks)), display();
-    },
-    finishTask = (t) => {
-        (tasks[t].status = "finished"), localStorage.setItem("tasks", JSON.stringify(tasks)), display();
-    },
-    clear = () => {
-        task.value = "";
-    },
-    addTask = (t) => {
-        console.log(t);
-        console.log(tasks);
-
-        if(tasks.find((e)=>{ return e.name=== t.name; })) {
-          
-            window.alert('this task is alredy exist');
-        }
-        else{
-        tasks.push(t),
-            localStorage.setItem("tasks", JSON.stringify(tasks)),
-            Toastify({
-                text: "task added successfully",
-                duration: 3e3,
-                destination: "https://github.com/apvarun/toastify-js",
-                newWindow: !0,
-                close: !0,
-                gravity: "top",
-                position: "right",
-                stopOnFocus: !0,
-                style: { background: "linear-gradient(to right, #00b09b, #96c93d)" },
-                onClick: function () {},
-            }).showToast()
-            }
-            clear(),
-           display();
-    };
-    const slugify = (name)=>{
-        name = name.toLowerCase();
-        name=name.replace(/ /g,'-');
-        return name;
-
-    }
-add.addEventListener("click", (e) => {
-    e.preventDefault();
-    const newTask = { name: task.value , slug : slugify(task.value) , status: "progress" };
-    addTask(newTask);
-}),
-    getTasks.addEventListener("click", (t) => {
-        t.preventDefault(), display();
-    }),
-    deleteAllTasks.addEventListener("click", (t) => {
-        t.preventDefault(),
-            (tasks = []),
-            Toastify({
-                text: "All tasks are deleted",
-                duration: 3e3,
-                destination: "https://github.com/apvarun/toastify-js",
-                newWindow: !0,
-                close: !0,
-                gravity: "top",
-                position: "right",
-                stopOnFocus: !0,
-                style: { background: "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))" },
-                onClick: function () {},
-            }).showToast(),
-            localStorage.clear("tasks"),
-            display();
-    }),
-    display();
+          </tr>`;document.getElementById("data").innerHTML=s},deleteTask=t=>{tasks.splice(t,1),localStorage.setItem("tasks",JSON.stringify(tasks)),display()},finishTask=t=>{tasks[t].status="finished",localStorage.setItem("tasks",JSON.stringify(tasks)),display()},clear=()=>{task.value=""},addTask=s=>{console.log(s),console.log(tasks),tasks.find(t=>t.name===s.name)?window.alert("this task is alredy exist"):(tasks.push(s),localStorage.setItem("tasks",JSON.stringify(tasks)),Toastify({text:"task added successfully",duration:3e3,destination:"https://github.com/apvarun/toastify-js",newWindow:!0,close:!0,gravity:"top",position:"right",stopOnFocus:!0,style:{background:"linear-gradient(to right, #00b09b, #96c93d)"},onClick:function(){}}).showToast()),clear(),display()},slugify=t=>t=(t=t.toLowerCase()).replace(/ /g,"-");add.addEventListener("click",t=>{t.preventDefault();t={name:task.value,slug:slugify(task.value),status:"progress"};addTask(t)}),getTasks.addEventListener("click",t=>{t.preventDefault(),display()}),deleteAllTasks.addEventListener("click",t=>{t.preventDefault(),tasks=[],Toastify({text:"All tasks are deleted",duration:3e3,destination:"https://github.com/apvarun/toastify-js",newWindow:!0,close:!0,gravity:"top",position:"right",stopOnFocus:!0,style:{background:"linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))"},onClick:function(){}}).showToast(),localStorage.clear("tasks"),display()}),display();
